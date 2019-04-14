9810a693dea0e73195aacb6a050a78c25b9a2dc1
redreaper752 <danialhasan14@gmail.com>
2019-04-14 17:28:15 -0400

    added navbar scroll color-change effect

--- jQuery.js
+++ jQuery.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function () {
+	$(window).scroll(function () {
+		var scroll = $(window).scrollTop();
+		if (scroll > 150) {
+			$("#navbar").css("background", "rgba(20, 20, 20, 0.8)");
+			$("#navbar").css("transition", "background-color 200ms linear");
+			$(".menuItem").css("color", "rgb(240, 240, 240)")
+		} else {
+			$("#navbar").css("background", "rgb(240, 240, 240)");
+			$(".menuItem").css("color", "rgb(20, 20, 20)")
+		}
+	})
+})
