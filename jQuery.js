8e21064477720caf36dd9370c819553ad6dcac49
Danial Hasan <danialhasan14@gmail.com>
2019-06-24 23:26:31 -0400

    message

--- jQuery.js
+++ jQuery.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-	$(window).scroll(function () {
-		var scroll = $(window).scrollTop();
-		if (scroll > 150) {
-			$("#navbar").css("background", "rgba(20, 20, 20, 0.8)");
-			$("#navbar").css("transition", "background-color 200ms linear");
-			$(".menuItem").css("color", "rgb(240, 240, 240)")
-		} else {
-			$("#navbar").css("background", "rgba(240, 240, 240, 0)");
-			$(".menuItem").css("color", "rgb(20, 20, 20)")
-		}
-	})
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 150) {
+            $("#navbar").css("background", "rgba(20, 20, 20, 0.8)");
+            $("#navbar").css("transition", "background-color 200ms linear");
+            $(".menuItem").css("color", "rgb(240, 240, 240)")
+        } else {
+            $("#navbar").css("background", "rgba(240, 240, 240, 0)");
+            //			$(".menuItem").css("color", "rgb(20, 20, 20)")
+            //            aw yeah
+        }
+    })
 })
