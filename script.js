41ac353222cd305467c351e1a0b87e35627dad9a
Danial Hasan <danialhasan14@gmail.com>
2019-07-23 01:47:18 -0400

    added slideshow with working buttons

--- script.js
+++ script.js
@@ -15,3 +15,44 @@ burger.addEventListener("click", function () { //when burger is clicked/menu is 
 function animateMenuBar(x) {
     x.classList.toggle("change");
 }
+
+var images = [
+    'Assets/image0.jpg',
+    'Assets/image1.jpg',
+    'Assets/image2.jpg',
+    'Assets/image3.jpg',
+    'Assets/image4.jpg']
+var carouselImage = document.getElementById("carouselImage");
+var previousButton = document.getElementById("previous");
+var nextButton = document.getElementById("next");
+var subtext = document.getElementsByClassName("featuredFoodSubtext");
+window.onload = function () {
+    var i = 0;
+    nextButton.addEventListener("click", function () {
+        if (i < images.length - 1) {
+            i++
+            carouselImage.src = images[i];
+            subtext.innerHTML = "Hello!"
+
+        } else {
+            i = 0;
+            carouselImage.src = images[i]
+        }
+        //        alert(i);
+
+    });
+    previousButton.addEventListener("click", function () {
+        if (i > 0) {
+            i--;
+            carouselImage.src = images[i];
+            //            alert(i);
+
+        } else if (i == 0) {
+            i = images.length - 1;
+            carouselImage.src = images[i];
+            //            alert(i);
+
+        }
+        subtext.innerHTML = i
+    })
+}
